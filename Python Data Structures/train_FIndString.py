3696c5904d6d2acf656391a9b73be9cc90cdd011
Aatechexe <mochammadasadurofiq@gmail.com>
2022-10-07 22:28:55 +0700

    change string

--- Python Data Structures/train_FIndString.py
+++ Python Data Structures/train_FIndString.py
@@ -1,5 +1,6 @@
 word = "Banana"
 pos = word.find("na")
+
 print(pos)
 
 aa = word.find("z")
--- Python Data Structures/train_StringLower.py
+++ Python Data Structures/train_StringLower.py
@@ -1,3 +1,4 @@
 word = 'My Name Is  Mikel JORDAN'
 print ('Sentence: ',word)
-print('Lower Setence:', word.lower())
+print('Lower Sentence:', word.lower())
+print('Lower Sentence:', word.upper())

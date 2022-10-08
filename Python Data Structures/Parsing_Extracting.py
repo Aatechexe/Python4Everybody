3c9a850edb338246515bc58a6bad38af3ad0168e
Aatechexe <mochammadasadurofiq@gmail.com>
2022-10-08 07:04:37 +0700

    new code

---
+++ Python Data Structures/Parsing_Extracting.py
@@ -0,0 +1,7 @@
+#Parsing word after @ till before space
+data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+atpos = data.find('@')
+
+sppos = data.find(" ",atpos)
+host = data[atpos+1:sppos]
+print(host)
---
+++ Python Data Structures/prefixes.py
@@ -0,0 +1,6 @@
+line  = 'Please have a nice day'
+line.startswith('Please')
+line.startswith('p')
+
+print(line.startswith("Please"))
+print(line.startswith('p'))

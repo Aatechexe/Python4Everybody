82664e13468aaf90f16726a85239c9ad07eb56c0
Aatechexe <mochammadasadurofiq@gmail.com>
2022-10-09 17:00:45 +0700

    new code

---
+++ Python Data Structures/Retrive_Key&Value.py
@@ -0,0 +1,6 @@
+data = {'chuck' : 1, 'fred' : 42, 'Jan' : 100}
+print(list(data))
+
+print(data.keys())
+print(data.values())
+print(data.items())
---
+++ Python Data Structures/create_dictionaries.py
@@ -0,0 +1,9 @@
+data = {'John': 32, 'fred' : 42, 'Jan':100}
+print(data)
+
+#chage value in dicti
+data ['John'] = 15
+print(data)
+
+blank = {}
+print(blank)

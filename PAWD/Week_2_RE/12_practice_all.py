e85d520a29b8d840ca8252ad2a5d3433f1199578
Aatechexe <mochammadasadurofiq@gmail.com>
2022-10-17 08:32:05 +0700

    new code

--- PAWD/Week_2_RE/12_practice_all.py
+++ PAWD/Week_2_RE/12_practice_all.py
@@ -4,7 +4,8 @@ numlist = list()
 for line in hand:
     line = line.rstrip()
     stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)',line)
+    print(stuff)
     if len(stuff) !=1 : continue
     num = float(stuff[0])
     numlist.append(num)
-print('Maximum: ', max(numlist))
+#print('Maximum: ', max(numlist))

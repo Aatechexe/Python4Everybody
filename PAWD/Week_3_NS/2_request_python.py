ceb0886c4db1f9dbf81626820695a7b687623764
Aatechexe <mochammadasadurofiq@gmail.com>
2022-10-19 08:28:47 +0700

    New change

--- PAWD/Week_3_NS/2_request_python.py
+++ PAWD/Week_3_NS/2_request_python.py
@@ -5,7 +5,7 @@ cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
-    data = mysock.recv(10)
+    data = mysock.recv(512)
     if (len(data) < 1):
         break  
     print(data.decode())
